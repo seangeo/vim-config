6199a4e1bd230ee6a4f4627e89c5960c29d80370
Sean Geoghegan <seangeo@gmail.com>
2024-04-17 01:17:14 +0930

    Add ,e shortcut for oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,5 +5,6 @@ return {
   config = function()
     require("oil").setup()
     vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+    vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Open parent directory" })
   end,
 }
