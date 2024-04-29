1127104e44025aab04205d3bb511035794224c17
Sean Geoghegan <seangeo@gmail.com>
2024-04-29 14:28:48 +0930

    Switch from Fugitive to Neogit.

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,6 +1,14 @@
 return {
   {
-    "tpope/vim-fugitive",
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+
+      -- Only one of these is needed, not both.
+      "nvim-telescope/telescope.nvim", -- optional
+    },
+    config = true
   },
   {
     "lewis6991/gitsigns.nvim",
